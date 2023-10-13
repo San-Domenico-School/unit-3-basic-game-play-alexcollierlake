8e71b7e1f9aac549bfca6c548629e378d30d09cb
alexcollierlake <112986612+alexcollierlake@users.noreply.github.com>
2023-10-13 11:45:51 -0700

    player Movement

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -3,38 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/*********
+ * component of the player, controls player movement.
+ * 
+ * Alexandra Collier-Lake
+ * 10/03/2005
+ **********/
+
+
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private GameObject projectile;
     private float speed;
-    private float centerToEdge;
+    private float centerToEdge = 35;
     private float move;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 3.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement(); //call playerMovement each frame
     }
 
     private void PlayerMovement()
     {
-        //to be implemented
+        
+        if(transform.position.x >= -centerToEdge && transform.position.x <= centerToEdge)
+        {
+            
+                transform.Translate(Vector3.right * Time.deltaTime * speed * move);
+            
+
+            //transform.position = new Vector3(centerToEdge, transform.position.y, transform.position.z);
+        }
+
     }
 
     private void OnMove(InputValue input)
     {
-        //to be implemented
+        Vector2 vector = input.Get<Vector2>();
+
+        move = vector.x;
+        Debug.Log(move);
     }
 
     private void OnFire()
     {
-        //to be implemented 
+        Instantiate(projectile, transform.position + Vector3.up, projectile.transform.rotation);
     }
 }
